e91d3643acd4afbd187c575fbe0fe2357411afbe
toufani1515 <toufani1515@gmail.com>
2019-08-28 21:35:08 +0800

    fix at models.py

--- apps/auth_admin/models.py
+++ apps/auth_admin/models.py
@@ -41,5 +41,5 @@ class Admin(db.Model):
 class Slider(db.Model):
     __tablename__ = "slider_img"
     id = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.String)
+    image = db.Column(db.String(100))
 
