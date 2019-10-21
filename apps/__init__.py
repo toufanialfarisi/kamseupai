3b26798fe8f7cb1177d8e544882535ead091e4f6
toufani1515 <toufani1515@gmail.com>
2019-10-21 10:16:15 +0800

    change db to psql

--- apps/__init__.py
+++ apps/__init__.py
@@ -20,7 +20,7 @@ db = SQLAlchemy(app)
 file_path = os.path.abspath(os.path.dirname(__file__))
 basedir = os.path.join(file_path, "data.sqlite")
 
-db_dev = "mysql"
+db_dev = "psql"
 if db_dev == "mysql":
     app.config[
         "SQLALCHEMY_DATABASE_URI"
