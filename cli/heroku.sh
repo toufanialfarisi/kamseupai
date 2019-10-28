4de36e3288c1d80cb6a0dda91ea5e846e513db33
toufani1515 <toufani1515@gmail.com>
2019-10-28 10:20:34 +0800

    add heroku

--- cli/heroku.sh
+++ cli/heroku.sh
@@ -1,1 +1,4 @@
 heroku config:set APP_SETTINGS=config.ProductionConfig
+heroku config:set FLASK_ENV=production
+heroku config:set HOST_MODE=production
+heroku config:set SECRET_KEY=kamseupai2019onfire17081945
