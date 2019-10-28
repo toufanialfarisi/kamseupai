c7f1c390203dfbbd4ea60b3c2da4db373b86f6eb
toufani1515 <toufani1515@gmail.com>
2019-10-28 10:34:56 +0800

    add heroku environment configuration

---
+++ cli/heroku/stag.sh
@@ -0,0 +1,4 @@
+heroku config:set APP_SETTINGS=config.StaggingConfig
+heroku config:set FLASK_ENV=development
+heroku config:set HOST_MODE=development
+heroku config:set SECRET_KEY=kamseupai2019onfire17081945
---
+++ cli/heroku/test.sh
@@ -0,0 +1,4 @@
+heroku config:set APP_SETTINGS=config.TestingConfig
+heroku config:set FLASK_ENV=development
+heroku config:set HOST_MODE=production
+heroku config:set SECRET_KEY=kamseupai2019onfire17081945
