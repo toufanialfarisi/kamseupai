7f7d18c952c23ef5d71f7f81d0e8412b072e253f
toufani1515 <toufani1515@gmail.com>
2019-10-29 23:19:14 +0800

    add some new google auth env

--- cli/heroku/prod.sh
+++ cli/heroku/prod.sh
@@ -2,3 +2,7 @@ heroku config:set APP_SETTINGS=config.ProductionConfig
 heroku config:set FLASK_ENV=production
 heroku config:set HOST_MODE=production
 heroku config:set SECRET_KEY=kamseupai2019onfire17081945
+heroku config:set GOOGLE_OAUTH_CLIENT_ID=1010640175796-vnp220hj8ceepbniguvrvifju99bi398.apps.googleusercontent.com
+heroku config:set GOOGLE_OAUTH_CLIENT_SECRET=5VBRMSPo32XiuL4N5h481zqJ
+heroku config:set OAUTHLIB_RELAX_TOKEN_SCOPE=true
+heroku config:set OAUTHLIB_INSECURE_TRANSPORT=true
